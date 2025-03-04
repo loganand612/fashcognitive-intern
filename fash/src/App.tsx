5c08f3a7d09139994cf9127235a2f36846015284
Gopi <gopim302004@gmail.com>
2025-03-04 15:45:04 +0530

    added dashboard and template page

--- fash/src/App.tsx
+++ fash/src/App.tsx
@@ -4,6 +4,8 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Home2 from './pages/Home2';
 import Create_template from './pages/Create_template';
+import Dashboard from './pages/Dashboard';
+import Template from './pages/Template_page';
 
 // import './App.css';zzz
 
@@ -16,6 +18,8 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/homee" element={<Home />} />
                 <Route path="/ct" element={<Create_template />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="template" element={<Template />} />
             </Routes>
         </Router>
     );
